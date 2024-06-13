498e40fc225d991410b21b72ea09d4c71f3c0c62
JulienBez <julienbezancon@gmail.com>
2024-06-13 17:00:07 +0200

    work in progress

--- DataCollect/MovieFinder/movieMain.py
+++ DataCollect/MovieFinder/movieMain.py
@@ -14,23 +14,48 @@ def createFolder(folder):
 
 def getDataset():
     "get the wanted dataset from url and extract it"
-    urllib.request.urlretrieve("https://datasets.imdbws.com/title.akas.tsv.gz", "MovieFinder/data/title.akas.tsv.gz")
-    with gzip.open("MovieFinder/data/title.akas.tsv.gz", 'rb') as f_in:
-        with open("MovieFinder/data/title.akas.tsv", 'wb') as f_out:
+    urllib.request.urlretrieve("https://datasets.imdbws.com/title.akas.tsv.gz", "data/title.akas.tsv.gz")
+    with gzip.open("data/title.akas.tsv.gz", 'rb') as f_in:
+        with open("data/title.akas.tsv", 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
 
 def getDictMovies():
     "read the imdb file and extract id, language and movie for each entry"
-    data = pd.read_csv("MovieFinder/data/title.akas.tsv",sep="\t")
+
+    data = pd.read_csv("data/title.akas.tsv",sep="\t")
     dict_movies = {}
+
+    counter = 0
+    file_counter = 0
+
+    previous_title = []
+    previous_titleId = data["titleId"][0]
+
     for i,titleId in enumerate(data["titleId"]):
-        if titleId not in dict_movies:
-            dict_movies[titleId] = {}
-        if data["language"][i] not in dict_movies[titleId]:
-            dict_movies[titleId][data["language"][i]] = data["title"][i]
-    with open('MovieFinder/data/dict_movies.json', 'w',encoding="utf'8") as f:
-        json.dump(dict_movies, f, indent=4, ensure_ascii=False)
+
+        if titleId != previous_titleId:
+            previous_title = []
+
+        title = data["title"][i]
+
+        if titleId+str(i) not in dict_movies and title not in previous_title:
+
+            dict_movies[titleId+str(i)] = {"title":title,"language":data["language"][i],"origin":"movie"}
+            previous_title.append(title)
+
+        counter += 1
+
+        if counter == 200000:
+
+            with open(f'data/split/movies_{file_counter}.json', 'w',encoding="utf'8") as f:
+                json.dump(dict_movies, f, indent=4, ensure_ascii=False)
+                
+            file_counter += 1
+            counter = 0
+            dict_movies = {}
+
+        previous_titleId = titleId
     
 
 def getLanguagesNumber(dict_movies):
@@ -42,31 +67,21 @@ def getLanguagesNumber(dict_movies):
     return len(languages)-1 # -1 to take into account the NULL value
 
 
-def applyTag():
-    "apply a simple tag to indicate from which dataset each title come from"
-    with open("MovieFinder/data/dict_movies.json",'r',encoding="utf-8") as f:
-        dict_movies = json.load(f)
-    for k,v in dict_movies.items():
-        v["origin"] = "movie"
-    with open('MovieFinder/data/dict_movies.json', 'w',encoding="utf'8") as f:
-        json.dump(dict_movies, f, indent=4, ensure_ascii=False)
-
-
 if __name__ == "__main__":
 
-    createFolder("MovieFinder/data")
-    if not os.path.exists("MovieFinder/data/title.akas.tsv"):
-        getDataset()
-        1/0
+    createFolder("data")
+    createFolder("data/split")
 
-    if not os.path.exists("MovieFinder/data/dict_movies.json"):
+    if not os.path.exists("data/title.akas.tsv"):
+        getDataset()
+
+    if not os.path.exists("data/dict_movies.json"):
         print("getting titles... (this might takes a while !)")
         getDictMovies()
-        applyTag()
-        print("titles saved in PressFinder/data/dict_movies.json !")
+        print("titles saved in data/dict_movies.json !")
 
     print("getting some basic metadata...")
-    with open("MovieFinder/data/dict_movies.json",'r',encoding="utf-8") as f:
+    with open("data/dict_movies.json",'r',encoding="utf-8") as f:
         dict_movies = json.load(f)
     print(f"number of titles : {len(dict_movies.keys())}")
     print(f"number of languages : {getLanguagesNumber(dict_movies)}")
