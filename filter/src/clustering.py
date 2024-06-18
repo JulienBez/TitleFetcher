1b08471b2f544ea633799cfb6ae300a38a3d73a2
JulienBez <julienbezancon@gmail.com>
2024-06-18 16:37:46 +0200

    clustering V1

--- filter/src/clustering.py
+++ filter/src/clustering.py
@@ -1,66 +1,21 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from scipy.sparse import csr_matrix
-import pandas as pd
-
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.pipeline import make_pipeline
-
 from sentence_transformers import SentenceTransformer, util
 
 from .navigation import *
 
 import time
 
-def vectorizer(titles):
-    "vectorize in unigrams and bigrams the seed and the tweet"
-    vectorizer = CountVectorizer(ngram_range=(1,2),stop_words=None,lowercase=False)
-    return vectorizer.fit_transform(titles)
-
-def test1():
-    titles = set()
-    #for path in glob.glob("data/languages/fr/*.json"):
-    for path in glob.glob("data/frtest/*.json"):
-        data = openJson(path)
-        for k,v in data.items():
-            titles.append(v["title"])
-    titles = list(titles)
-
-    start = time.time()
-
-    X = vectorizer(titles)
-    X_sparse = csr_matrix(X)
-
-    # Step 2: Clustering with MiniBatchKMeans
-    n_clusters = 1000  # Example number of clusters
-    batch_size = 10000
-    kmeans = MiniBatchKMeans(n_clusters=n_clusters, batch_size=batch_size)
-    kmeans.fit(X)
-
-    # Access cluster labels
-    cluster_labels = kmeans.labels_
-
-    cluster_dict = {}
-    for idx, title in enumerate(titles):
-        cluster_label = cluster_labels[idx]
-        if cluster_label not in cluster_dict:
-            cluster_dict[str(cluster_label)] = []
-        cluster_dict[str(cluster_label)].append(title)
-    writeJson("temp/clusters.json",cluster_dict)
-
-    end = time.time()
-    print(f"executed in {round(end - start,2)}")
-
 
 #https://github.com/UKPLab/sentence-transformers/blob/master/examples/applications/clustering/fast_clustering.py
 def test2():
 
+    #TODO1 : au lieu de faire ça on peut faire [ [id,titre,langue,origine], [id,titre,langue,origine], ... ]
     titles = set()
     #for path in glob.glob("data/languages/fr/*.json"):
     for path in glob.glob("data/frtest/*.json"):
         data = openJson(path)
         for k,v in data.items():
             titles.add(v["title"])
-    titles = list(titles)
+    titles = list(titles) #TODO1 : et là on fait titles = [i[1] for i in notre_liste]
 
     start = time.time()
 
@@ -80,10 +35,11 @@ def test2():
     end = time.time()
     print(f"clustering executed in {round(end - start,2)}")
 
+    dict_cluster = {}
     for i, cluster in enumerate(clusters):
-        print("\nCluster {}, #{} Elements ".format(i + 1, len(cluster)))
-        for sentence_id in cluster[0:3]:
-            print("\t", titles[sentence_id])
-        print("\t", "...")
-        for sentence_id in cluster[-3:]:
-            print("\t", titles[sentence_id])
+        print("Cluster {}, #{} Elements ".format(i , len(cluster)))
+        if str(i) not in dict_cluster:
+            dict_cluster[str(i)] = []
+        for sentence_id in cluster:
+            dict_cluster[str(i)].append(titles[sentence_id])
+    writeJson("test.json",dict_cluster)
